c92ada17f922240481f6ed703fa5813d6fdb4f8d
Vidoalessandro <gabriellevido17@gmail.com>
2024-04-21 21:38:35 -0300

    User register

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
 
 # Create your views here.
 @csrf_protect
-def helloworld(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm,
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # register user
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created succesfully')
+            except:
+                return HttpResponse('Username already exists')
+        return HttpResponse('Passwords dont match')
